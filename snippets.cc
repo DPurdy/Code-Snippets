c8876b0f7359b4e5da83175692ce9cdf58cd4159
DPurdy <89486262+DPurdy@users.noreply.github.com>
2021-09-15 17:00:56 -0400

    Add files via upload

---
+++ snippets.cc
@@ -0,0 +1,22 @@
+/**
+*   @file: //file name
+* @author: Dan Purdy
+*   @date: //date
+*  @brief: //description
+*/
+    
+    
+#include <stdio.h>
+#include <ctype.h>
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+    
+int main (int argc, char const *argv)
+{
+    
+//code
+    
+return 0;
+}
